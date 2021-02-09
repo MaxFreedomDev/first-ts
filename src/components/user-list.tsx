57d4e04107b725da30440bed3ba92cc8bfda0b19
MadMax <maxviper89@gmail.com>
2021-02-09 17:01:54 +0200

    add hook use-action

--- src/components/user-list.tsx
+++ src/components/user-list.tsx
@@ -1,15 +1,15 @@
 import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/use-typed-selector";
-import {useDispatch} from "react-redux";
-import {fetchUsers} from "../store/action-creators/user";
+import {useActions} from "../hooks/use-action";
 
 const UserList: React.FC = () => {
     const { users, error, loading } = useTypedSelector(state => state.user);
-    const dispatch = useDispatch();
+    const {fetchUsers} = useActions();
 
     useEffect(()=>{
-        dispatch(fetchUsers());
-    }, [dispatch]);
+        fetchUsers();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     if (loading) {
         return <h1>Loading ....</h1>
