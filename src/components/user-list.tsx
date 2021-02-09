e3863ac8d5a881666952c93bd5a1925d9dca0e4e
MadMax <maxviper89@gmail.com>
2021-02-09 16:55:11 +0200

    add users

---
+++ src/components/user-list.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect} from 'react';
+import {useTypedSelector} from "../hooks/use-typed-selector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../store/action-creators/user";
+
+const UserList: React.FC = () => {
+    const { users, error, loading } = useTypedSelector(state => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if (loading) {
+        return <h1>Loading ....</h1>
+    }
+
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
+
+    return (
+        <div>
+            {users.map( user => (
+                <div key={user.id}>{user.name}</div>
+            ))}
+        </div>
+    );
+};
+
+export default UserList;
